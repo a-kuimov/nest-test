850f82467bcaca6c4e2d33e57cf97b74ce17b1aa
a-kuimov <a_kuimov@inbox.ru>
2024-04-22 18:52:10 +0700

    connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '37.143.9.134',
+      port: 3306,
+      username: 'uts',
+      password: 'uts123UTS!@#',
+      database: 'uts',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
