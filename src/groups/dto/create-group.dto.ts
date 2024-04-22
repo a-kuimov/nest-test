27120fd016989de1075eaecc4fc1abde021c6bce
a-kuimov <a_kuimov@inbox.ru>
2024-04-22 20:56:42 +0700

    complite controller and service of group

---
+++ src/groups/dto/create-group.dto.ts
@@ -0,0 +1,4 @@
+export class CreateGroupDto {
+  readonly id: number;
+  readonly name: string;
+}
--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { GroupsService } from './groups.service';
+import { CreateGroupDto } from './dto/create-group.dto';
 
 @Controller('groups')
-export class GroupsController {}
+export class GroupsController {
+  constructor(private readonly groupsService: GroupsService) {}
+
+  @Post()
+  async create(@Body() createGroupDto: CreateGroupDto[]) {
+    return await this.groupsService.create(createGroupDto);
+  }
+}
