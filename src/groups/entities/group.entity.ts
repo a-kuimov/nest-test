011195fdde9004644c572b0a6f746d4b02dbb595
a-kuimov <a_kuimov@inbox.ru>
2024-04-22 19:51:26 +0700

    replace the group entity

---
+++ src/groups/entities/group.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+// import { Task } from '../../tasks/entities/task.entity';
+
+@Entity()
+export class Group {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  // @OneToMany(() => Task, (task) => task.group)
+  // tasks: Task[];
+}
