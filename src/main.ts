c9087d0d2d2f0a8821c6ddbb96cabc4481180b46
a-kuimov <a_kuimov@inbox.ru>
2024-04-23 23:40:21 +0700

    erer

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://cdn-ru.bitrix24.ru/',
+      'https://cdn-ru.bitrix24.ru',
+      'http://localhost:5000',
+    ],
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Authorization',
+    ],
+    credentials: true,
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(5019);
