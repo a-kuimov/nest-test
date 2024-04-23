f6f77c8b5af37e496e70ec699cbcaee484ab605f
a-kuimov <a_kuimov@inbox.ru>
2024-04-23 18:23:55 +0700

    change cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(5019);
