1db552cc7b0f62f8f497c004cd4017eafb6e6a9b
a-kuimov <a_kuimov@inbox.ru>
2024-04-24 09:20:02 +0700

    try

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,7 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://cdn-ru.bitrix24.ru/',
-      'https://cdn-ru.bitrix24.ru',
-      'http://localhost:5000',
-    ],
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: [
-      'Content-Type',
-      'Origin',
-      'X-Requested-With',
-      'Accept',
-      'Authorization',
-    ],
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(5019);
