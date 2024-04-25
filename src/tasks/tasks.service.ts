a0c5629b03075085b32002d8495175106df4da12
a-kuimov <a_kuimov@inbox.ru>
2024-04-25 10:13:03 +0700

    delete first when add data to task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,12 @@ export class TasksService {
     private taskRepository: Repository<Task>,
   ) {}
   async create(createTaskDto: CreateTaskDto[]) {
+    const id = createTaskDto[0].groupId;
+    await this.taskRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Task)
+      .where('id = :id', { id: id });
     return await this.taskRepository.save(createTaskDto);
   }
 }
